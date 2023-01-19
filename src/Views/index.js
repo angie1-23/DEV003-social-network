bf09f31003e80b52bc5aa23d5d4bcd65adbbf5fb
mafcht <mfernanda.cht@gmail.com>
2023-01-19 16:46:31 -0600

    cambiosss

--- src/Views/index.js
+++ src/Views/index.js
@@ -3,6 +3,7 @@ import start from './startHome.js';
 import login from './login.js';
 import different from './404.js';
 import email from './loginEm.js';
+import google from './google';
 import register from './register.js';
 import home from './home.js';
 import post from './post.js';
@@ -13,6 +14,7 @@ const components = {
   Login: login,
   Different: different,
   Email: email,
+  Google: google,
   Register: register,
   Home: home,
   Post: post,
